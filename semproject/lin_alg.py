ee53f8131bddc6fef8c7c42250d7854193d6466e
Alexandre Koroneos <akoroneos@outlook.de>
2024-11-26 19:12:20 +0100

    Add example code from course repo template

---
+++ semproject/lin_alg.py
@@ -0,0 +1,5 @@
+def py_mul(a, b):
+    """Fallback function"""    
+    return a * b
+
+from ._lin_alg import *
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
+#include <iostream>
 
-int main() {
-    return 0;
+#include "DenseVector.hpp"
+
+int main(int, char**){
+    DenseVector v{1,2,3,4};
+    auto u = v * 3;
+    u.print();
+    bool same = (u == v);
+    std::cout << "Same: " << same << "\n";
 }
-
