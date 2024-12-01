7934ecc262898a048b20c56247329aa0834d7678
Marios Spathiadis <marios.spathiadis@gmail.com>
2024-12-01 15:48:22 +0100

    Add test for Example.cpp

--- src/tests/ExampleTest.cpp
+++ src/tests/ExampleTest.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include "Example.hpp"
 
-int main() {
-	return 0;
+class ExampleTest : public testing::Test {};
+
+TEST(ExampleTest, getString) {
+	Example example("Hello World");
+	EXPECT_EQ(example.getString(), "Hello World");
+	EXPECT_NE(example.getString(), "Hello");
+}
+int main(int, char**){
+	
+	::testing::InitGoogleTest();
+
+	std::cout << "Starting unit tests.\n";
+	return RUN_ALL_TESTS();
 }
