10918a325cb0675524048012306f07d4e955cf3f
Bshara Jahshan <bsharajahshan07@gmail.com>
2024-12-08 14:43:00 +0100

    Add test for is_valid_node function

--- src/tests/GraphTest.cpp
+++ src/tests/GraphTest.cpp
@@ -86,6 +86,16 @@ TEST(GraphTest, has_edge) {
     EXPECT_FALSE(graph.has_edge(0, 2));  // Assumes edge does not exist
 }
 
+
+TEST(GraphTest, is_valid_node) {
+    std::string edges_path = "../input/Gtests/edges_example.txt";
+    std::string features_path = "../input/Gtests/features_example.txt";
+    Graph graph(edges_path, features_path);
+
+    EXPECT_TRUE(graph.is_valid_node(0));
+    EXPECT_FALSE(graph.is_valid_node(graph.get_num_nodes()));
+}
+
 TEST(GraphTest, read_edges) {
     // TODO: only check first and last lines of the files for quicker tests
     std::string edges_path = "../input/amazon/amazon_edges.txt";
