ffdb1bb9e7108543b6796cbfa5cbd4115355c08a
Alexandre Koroneos <akoroneos@outlook.de>
2024-12-12 18:16:06 +0100

    Update GraphTest to changes in Graph interface

--- src/tests/GraphTest.cpp
+++ src/tests/GraphTest.cpp
@@ -12,12 +12,14 @@ TEST(GraphTest, FileNotOpenThrowsError) {
     std::string invalid_file_path = "non_existent_file.txt";
 
     // Verify that the function throws a runtime_error
-    EXPECT_THROW({
-        std::ifstream file(invalid_file_path);
-        if (!file.is_open()) {
-            throw std::runtime_error("Could not open file");
-        }
-    }, std::runtime_error);
+    EXPECT_THROW(
+        {
+            std::ifstream file(invalid_file_path);
+            if (!file.is_open()) {
+                throw std::runtime_error("Could not open file");
+            }
+        },
+        std::runtime_error);
 }
 
 TEST(GraphTest, parse_node_count) {
@@ -37,49 +39,22 @@ TEST(GraphTest, parse_feature_count) {
     EXPECT_EQ(parse_feature_count(amazon_fraud_features), 25 + 1);
 }
 
-TEST(GraphTest, get_features) {
-    std::string edges_path = "../input/Gtests/edges_example.txt";
-    std::string features_path = "../input/Gtests/features_example.txt";
-    Graph graph(edges_path, features_path);
-
-    std::vector<double> features = graph.get_features(0);
-    std::vector<double> expected_features = {1, 0, 0, 0, 0, 0, 2};
-
-    
-    EXPECT_EQ(features, expected_features);
-}
-
-TEST(GraphTest, get_missing_features) {
-    std::string edges_path = "../input/Gtests/edges_example.txt";
-    std::string features_path = "../input/Gtests/features_example.txt";
-    Graph graph(edges_path, features_path);
-
-    std::vector<bool> missing_features = graph.get_missing_features(0);
-    std::vector<bool> expected_missing_features = {0,0,1,0,0,0,0};
-    
-
-    EXPECT_EQ(missing_features, expected_missing_features);
- 
-}
-
 TEST(GraphTest, get_neighbours) {
     std::string edges_path = "../input/Gtests/edges_example.txt";
     std::string features_path = "../input/Gtests/features_example.txt";
     Graph graph(edges_path, features_path);
 
     std::vector<int> neighbours = graph.get_neighbours(0);
-    std::vector<int> expected_neighbours = {1,4};
+    std::vector<int> expected_neighbours = {1, 4};
 
     EXPECT_EQ(neighbours, expected_neighbours);
 
     // Test for a specific depth
     std::vector<int> depth_neighbours = graph.get_neighbours(0, 2);
-    std::vector<int> expected_depth_neighbours = {1,4,0,2,3};
+    std::vector<int> expected_depth_neighbours = {1, 4, 0, 2, 3};
 
     EXPECT_EQ(depth_neighbours, expected_depth_neighbours);
-    
 }
-
 
 TEST(GraphTest, get_degree) {
     std::string edges_path = "../input/Gtests/edges_example.txt";
@@ -95,10 +70,9 @@ TEST(GraphTest, has_edge) {
     std::string features_path = "../input/Gtests/features_example.txt";
     Graph graph(edges_path, features_path);
 
-    EXPECT_TRUE(graph.has_edge(0, 4));  // Assumes edge exists
+    EXPECT_TRUE(graph.has_edge(0, 4));   // Assumes edge exists
     EXPECT_FALSE(graph.has_edge(0, 2));  // Assumes edge does not exist
 }
-
 
 TEST(GraphTest, is_valid_node) {
     std::string edges_path = "../input/Gtests/edges_example.txt";
@@ -108,7 +82,6 @@ TEST(GraphTest, is_valid_node) {
     EXPECT_TRUE(graph.is_valid_node(0));
     EXPECT_FALSE(graph.is_valid_node(graph.get_num_nodes()));
 }
-
 
 TEST(GraphTest, print_edges) {
     std::string edges_path = "../input/Gtests/edges_example.txt";
@@ -121,11 +94,11 @@ TEST(GraphTest, print_edges) {
 
     // Call the function to capture its output
     graph.print_edges();
-    
+
     // Restore std::cout
     std::cout.rdbuf(oldCoutBuffer);
 
-    std::string expected_print = 
+    std::string expected_print =
         "1\t0\n"
         "2\t1\n"
         "3\t2\n"
@@ -150,14 +123,14 @@ TEST(GraphTest, print_features) {
     // Restore std::cout
     std::cout.rdbuf(oldCoutBuffer);
 
-    std::string expected_print = 
-    "0\t1, 0, '#', 0, 0, 0\t2\n"
-    "1\t0, 1, '#', 0, 0, 0\t0\n"
-    "2\t0, 0, '#', 1, 0, 0\t1\n"
-    "3\t0, 0, '#', 0, 1, 0\t3\n"
-    "4\t0, 0, '#', 0, 0, 1\t4\n";
+    std::string expected_print =
+        "0\t1, 0, '#', 0, 0, 0\t2\n"
+        "1\t0, 1, '#', 0, 0, 0\t0\n"
+        "2\t0, 0, '#', 1, 0, 0\t1\n"
+        "3\t0, 0, '#', 0, 1, 0\t3\n"
+        "4\t0, 0, '#', 0, 0, 1\t4\n";
 
-    EXPECT_EQ(output.str(),expected_print);
+    EXPECT_EQ(output.str(), expected_print);
 }
 
 TEST(GraphTest, read_edges) {
@@ -191,12 +164,6 @@ TEST(GraphTest, read_edges) {
         }
     }
 
-    std::vector<int> expected_offsets = {0,2,4,6,8,10};
-    std::vector<int> expected_edges = {1,4,0,2,1,3,2,4,3,0};
-
-    EXPECT_EQ(graph.get_offsets(), expected_offsets);
-    EXPECT_EQ(graph.get_edges(),expected_edges);
-
     EXPECT_TRUE(files_match) << "Files differ at line " << line_number;
 
     bool more_lines = std::getline(file1, line1) || std::getline(file2, line2);
@@ -206,31 +173,31 @@ TEST(GraphTest, read_edges) {
     std::remove(temp_file_path.c_str());
 }
 
-TEST(GraphTest, read_features) {  // TODO: How to test this when formatting is different
+TEST(GraphTest, read_features) {
     std::string edges_path = "../input/Gtests/edges_example.txt";
     std::string features_path = "../input/Gtests/features_example.txt";
     Graph graph(edges_path, features_path);
 
-
-    std::vector<std::vector<double>> expected_features = {
-        {1,0,0,0,0,0,2},
-        {0,1,0,0,0,0,0},
-        {0,0,0,1,0,0,1},
-        {0,0,0,0,1,0,3},
-        {0,0,0,0,0,1,4}
+    // clang-format off
+    std::vector<std::vector<double>> actual_features = {
+        {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2},
+        {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0},
+        {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1},
+        {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 3},
+        {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 4}
     };
+    // clang-format on
 
-    std::vector<bool> expected_missing = {0,0,1,0,0,0,0};
-
-    for(int i = 0; i < graph.get_num_nodes() - 1; i++) {
-        EXPECT_EQ(graph.get_features(i), expected_features[i]);
-        EXPECT_EQ(graph.get_missing_features(i), expected_missing);
+    for (int node = 0; node < graph.get_num_nodes(); node++) {
+        for (int feature = 0; feature < graph.get_num_features(); feature++) {
+            if (!graph.is_missing(node, feature)) {
+                EXPECT_DOUBLE_EQ(graph.get_feature(node, feature), actual_features[node][feature]);
+            }
+        }
     }
-       
-    
 }
 
-TEST(UtilityTest, RemoveDuplicates) {
+TEST(UtilityTest, remove_duplicates) {
     // Test Case 1: Vector with duplicates
     std::vector<int> input1 = {1, 2, 3, 2, 4, 3, 5};
     std::vector<int> expected1 = {1, 2, 3, 4, 5};
@@ -245,7 +212,6 @@ TEST(UtilityTest, RemoveDuplicates) {
     std::vector<int> input4 = {10, 10, 10, 10};
     std::vector<int> expected4 = {10};
     EXPECT_EQ(remove_duplicates(input4), expected4);
-
 }
 
 int main(int, char**) {
