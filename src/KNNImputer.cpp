87faacfda424056a1130074ad66a9bd06c63b432
Marios Spathiadis <marios.spathiadis@gmail.com>
2024-12-15 12:40:45 +0100

    Add Hashmap to store global averages

--- src/KNNImputer.cpp
+++ src/KNNImputer.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
 
 #include "Graph.hpp"
 
@@ -15,6 +16,8 @@ void KNNImputer::run() {
     int num_nodes = graph.get_num_nodes();
     int num_features = graph.get_num_features();
 
+    std::unordered_map<int, double> global_averages;  // stores global averages for each feature
+
     // for each node in graph if feature is missing take neighbours depth k and take average of
     // features
     for (int node = 0; node < num_nodes; node++) {
@@ -24,6 +27,7 @@ void KNNImputer::run() {
         for (auto&& feature : missing_features) {
             double sum = 0;
             int count = 0;
+
             for (int neighbour : neighbours) {
                 if (!graph.is_missing(neighbour, feature)) {
                     sum += graph.get_feature(neighbour, feature);
@@ -33,15 +37,21 @@ void KNNImputer::run() {
 
             if (count > 0) {
                 graph.set_feature(node, feature, sum / count);
-            } else {  // neighbourhood also doesn't have this feature
-                double global_avg = compute_global_average(graph, feature);
-                graph.set_feature(node, feature, global_avg);
+            } else {  // if no neighbours have the feature, use global average
+                auto it = global_averages.find(feature);
+                if (it != global_averages.end()) {
+                    graph.set_feature(node, feature, it->second);
+                } else {
+                    double global_avg = compute_global_average(graph, feature);
+                    graph.set_feature(node, feature, global_avg);
+                    global_averages[feature] = global_avg;
+                }
             }
             graph.set_missing(node, feature, false);
         }
     }
 }
-
+// Compute the global average of a feature across all nodes in the graph.
 double compute_global_average(Graph& graph, int feature) {
     double sum = 0;
     int count = 0;
@@ -50,6 +60,10 @@ double compute_global_average(Graph& graph, int feature) {
             sum += graph.get_feature(node, feature);
             count++;
         }
+    }
+    // check if every node has missing feature and return 0 if so
+    if (count == 0) {
+        return 0;
     }
     return sum / count;
 }
