12080a82b25b9fe4527c4f2e01a4e229fcf93066
Marios Spathiadis <marios.spathiadis@gmail.com>
2024-12-15 23:20:01 +0100

    Add tests for depth and global average

--- src/tests/KNNImputerTest.cpp
+++ src/tests/KNNImputerTest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 
@@ -40,6 +41,8 @@ TEST(KNNTest, testKNN) {
         line_number++;
         if (line1 != line2) {
             files_match = false;
+            printf("line1: %s\n", line1.c_str());
+            printf("line2: %s\n", line2.c_str());
             break;
         }
     }
@@ -51,6 +54,40 @@ TEST(KNNTest, testKNN) {
 
     // Clean up
     std::remove(temp_file_path.c_str());
+}
+TEST(KNNTest, testDepth) {
+    std::string edges_path = "../input/test/test_edges.txt";
+    std::string features_path = "../input/test/test_missing_features.txt";
+    Graph graph(edges_path, features_path);
+    KNNImputer knn(graph);
+    knn.set_depth(3);
+    EXPECT_EQ(knn.get_depth(), 3);
+    EXPECT_NE(knn.get_depth(), 2);
+    knn.set_depth(5);
+    EXPECT_EQ(knn.get_depth(), 5);
+    EXPECT_NE(knn.get_depth(), 3);
+}
+TEST(KNNTest, testGlobalAverage) {
+    std::setprecision(5);
+    std::string edges_path = "../input/test/test_edges.txt";
+    std::string features_path = "../input/test/test_missing_features.txt";
+    Graph graph(edges_path, features_path);
+    KNNImputer knn = KNNImputer(graph);
+    double average0 = compute_global_average(graph, 0);
+    double average1 = compute_global_average(graph, 1);
+    double average2 = compute_global_average(graph, 2);
+
+    EXPECT_EQ(average0, 1);
+    EXPECT_EQ(average1, 2);
+    EXPECT_NEAR(average2, 2.33333, 1e-5);
+    knn.set_depth(3);
+    knn.run();
+    average0 = compute_global_average(graph, 0);
+    average1 = compute_global_average(graph, 1);
+    average2 = compute_global_average(graph, 2);
+    EXPECT_NEAR(average0, 1.08, 1e-5);
+    EXPECT_EQ(average1, 2);
+    EXPECT_NEAR(average2, 2.33333, 1e-5);
 }
 int main(int, char**) {
     ::testing::InitGoogleTest();
