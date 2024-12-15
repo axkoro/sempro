bbc0ed37a9487685bd0e5ae44787f47988703686
Marios Spathiadis <marios.spathiadis@gmail.com>
2024-12-15 18:59:01 +0100

    Add amazon_fraud benchmark

--- semproject/benchmarks.py
+++ semproject/benchmarks.py
@@ -15,7 +15,7 @@ def graph_benchmark():
     print(f"Graph operations completed in {end_time - start_time:.4f} seconds")
 
 def kNN_twitch_benchmark():
-    print("Running KNNImputer tests...")
+    print("Running kNN twitch benchmark with depth 3...")
     
     # Create a Graph instance
     edges_path = "../input/twitch/twitch_edges.txt"
@@ -31,8 +31,27 @@ def kNN_twitch_benchmark():
     knn_imputer.run()
     end_time = time.time()
     
-    print(f"KNNImputer run completed in {end_time - start_time:.4f} seconds")
+    print(f"kNN twitch benchmark completed in {end_time - start_time:.4f} seconds")
+
+def kNN_fraud_benchmark():
+    print("Running kNN amazon_fraud benchmark with depth 3...")
+    
+    # Create a Graph instance
+    edges_path = "../input/amazon_fraud/amazon_fraud_edges.txt"
+    features_path = "../input/amazon_fraud/amazon_fraud_features.txt"
+    graph = graph_module.Graph(edges_path,features_path)
+    
+    # Create a KNNImputer instance
+    knn_imputer = strats_module.KNNImputer(graph)
+    knn_imputer.set_depth(3)
+    
+    # Run the KNNImputer
+    start_time = time.time()
+    knn_imputer.run()
+    end_time = time.time()
+    
+    print(f"kNN amazon_fraud benchmark completed in {end_time - start_time:.4f} seconds")
 
 if __name__ == "__main__":
     graph_benchmark()
-    kNN_twitch_benchmark()
+    kNN_fraud_benchmark()
