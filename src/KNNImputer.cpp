3861deca112860320f57d7b9369d4247e7b38cc1
Alexandre Koroneos <akoroneos@outlook.de>
2025-01-06 19:30:58 +0100

    Add mutex for thread-safe global average computation in KNNImputer

--- src/KNNImputer.cpp
+++ src/KNNImputer.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <unordered_map>
 
@@ -13,6 +14,7 @@
 KNNImputer::KNNImputer(Graph& g) : Imputer(g) {}
 
 void KNNImputer::set_depth(int k) { this->k = k; }
+int KNNImputer::get_depth() { return k; }
 
 void KNNImputer::run() {
     int num_nodes = graph.get_num_nodes();
@@ -20,13 +22,13 @@ void KNNImputer::run() {
 
     std::unordered_map<int, double> global_averages;  // stores global averages for each feature
 
-// for each node in graph if feature is missing take neighbours depth k and take average of
-// features
 #pragma omp parallel for
+    // for each node in graph if feature is missing take the average of this feature among the
+    // neighbourhood with depth k
     for (int node = 0; node < num_nodes; node++) {
         std::vector<int> neighbours = graph.get_neighbours(node, k);
-        std::vector<int> missing_features = graph.get_missing_features(node);
 
+        std::vector<int> missing_features = graph.get_missing_features(node);
         for (auto&& feature : missing_features) {
             double sum = 0;
             int count = 0;
@@ -47,13 +49,21 @@ void KNNImputer::run() {
                 } else {
                     double global_avg = compute_global_average(graph, feature);
                     graph.set_feature(node, feature, global_avg);
-                    global_averages[feature] = global_avg;
+
+                    std::mutex avg_mutex;
+                    {
+                        std::lock_guard<std::mutex> lock(avg_mutex);
+                        global_averages[feature] = global_avg;
+                    }
                 }
             }
+
+            // this leads to imputed features being used for further imputations
             graph.set_missing(node, feature, false);
         }
     }
 }
+
 // Compute the global average of a feature across all nodes in the graph.
 double compute_global_average(Graph& graph, int feature) {
     double sum = 0;
@@ -64,11 +74,6 @@ double compute_global_average(Graph& graph, int feature) {
             count++;
         }
     }
-    // check if every node has missing feature and return 0 if so
-    if (count == 0) {
-        return 0;
-    }
-    return sum / count;
+
+    return (count > 0) ? (sum / count) : 0;
 }
-// getter for k
-int KNNImputer::get_depth() { return k; }
