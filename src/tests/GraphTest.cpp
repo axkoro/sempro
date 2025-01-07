aeb2979cc36c141e4f500bda52225d008c84041d
Alexandre Koroneos <akoroneos@outlook.de>
2025-01-07 20:27:37 +0100

    Add tests for different graph types

--- src/tests/GraphTest.cpp
+++ src/tests/GraphTest.cpp
@@ -33,10 +33,223 @@ TEST(GraphTest, parse_node_count) {
 
 TEST(GraphTest, parse_feature_count) {
     std::string github_features = "../input/github/github_features.txt";
-    EXPECT_EQ(parse_feature_count(github_features), 128 + 1);
+    EXPECT_EQ(parse_feature_count(github_features), 128);
 
     std::string amazon_fraud_features = "../input/amazon_fraud/amazon_fraud_features.txt";
-    EXPECT_EQ(parse_feature_count(amazon_fraud_features), 25 + 1);
+    EXPECT_EQ(parse_feature_count(amazon_fraud_features), 25);
+}
+
+// TODO: Currently checks reading of both edges and features (should only load edges)
+TEST(GraphTest, read_edges) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<double> graph(edges_path, features_path);
+
+    // Create temporary file for output
+    std::string temp_file_path = "temp_edges.txt";
+    {
+        std::ofstream output(temp_file_path);
+        // Redirect cout to the file
+        std::streambuf* old = std::cout.rdbuf(output.rdbuf());
+        graph.print_edges();
+        std::cout.rdbuf(old);
+    }
+
+    // Compare files line by line
+    std::ifstream file1(edges_path);
+    std::ifstream file2(temp_file_path);
+    std::string line1, line2;
+    bool files_match = true;
+    int line_number = 0;
+
+    while (std::getline(file1, line1) && std::getline(file2, line2)) {
+        line_number++;
+        if (line1 != line2) {
+            files_match = false;
+            break;
+        }
+    }
+
+    EXPECT_TRUE(files_match) << "Files differ at line " << line_number;
+
+    bool more_lines = std::getline(file1, line1) || std::getline(file2, line2);
+    EXPECT_FALSE(more_lines) << "Files have different lengths";
+
+    // Clean up
+    std::remove(temp_file_path.c_str());
+}
+
+TEST(GraphTest, read_bool_features) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<bool> graph(edges_path, features_path);
+
+    // clang-format off
+    std::vector<std::vector<double>> actual_features = {
+        {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2},
+        {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0},
+        {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1},
+        {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 3},
+        {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 4}
+    };
+    // clang-format on
+
+    for (int node = 0; node < graph.get_num_nodes(); node++) {
+        for (int feature = 0; feature < graph.get_num_features(); feature++) {
+            // TODO: also check if the correct features are missing
+            if (!graph.is_missing(node, feature)) {
+                EXPECT_EQ(graph.get_feature(node, feature), actual_features[node][feature]);
+            }
+        }
+    }
+}
+
+TEST(GraphTest, read_int_features) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<int> graph(edges_path, features_path);
+
+    // clang-format off
+    std::vector<std::vector<double>> actual_features = {
+        {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2},
+        {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0},
+        {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1},
+        {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 3},
+        {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 4}
+    };
+    // clang-format on
+
+    for (int node = 0; node < graph.get_num_nodes(); node++) {
+        for (int feature = 0; feature < graph.get_num_features(); feature++) {
+            if (!graph.is_missing(node, feature)) {
+                EXPECT_EQ(graph.get_feature(node, feature), actual_features[node][feature]);
+            }
+        }
+    }
+}
+
+TEST(GraphTest, read_double_features) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<double> graph(edges_path, features_path);
+
+    // clang-format off
+    std::vector<std::vector<double>> actual_features = {
+        {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2},
+        {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0},
+        {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1},
+        {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 3},
+        {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 4}
+    };
+    // clang-format on
+
+    for (int node = 0; node < graph.get_num_nodes(); node++) {
+        for (int feature = 0; feature < graph.get_num_features(); feature++) {
+            if (!graph.is_missing(node, feature)) {
+                EXPECT_DOUBLE_EQ(graph.get_feature(node, feature), actual_features[node][feature]);
+            }
+        }
+    }
+}
+
+TEST(GraphTest, print_edges) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<double> graph(edges_path, features_path);
+
+    // Redirect std::cout to capture the output
+    std::ostringstream output;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+    // Call the function to capture its output
+    graph.print_edges();
+
+    // Restore std::cout
+    std::cout.rdbuf(oldCoutBuffer);
+
+    std::string expected_print =
+        "1\t0\n"
+        "2\t1\n"
+        "3\t2\n"
+        "4\t3\n"
+        "4\t0\n";
+
+    EXPECT_EQ(output.str(), expected_print);
+}
+
+TEST(GraphTest, print_bool_features) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<bool> graph(edges_path, features_path);
+
+    // Redirect std::cout to capture the output
+    std::ostringstream output;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+    // Call the function to capture its output
+    graph.print_features();
+
+    // Restore std::cout
+    std::cout.rdbuf(oldCoutBuffer);
+
+    std::string expected_print =
+        "0\t1, 0, '#', 0, 0, 0\t2\n"
+        "1\t0, 1, '#', 0, 0, 0\t0\n"
+        "2\t0, 0, '#', 1, 0, 0\t1\n"
+        "3\t0, 0, '#', 0, 1, 0\t3\n"
+        "4\t0, 0, '#', 0, 0, 1\t4\n";
+
+    EXPECT_EQ(output.str(), expected_print);
+}
+
+TEST(GraphTest, print_int_features) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<int> graph(edges_path, features_path);
+
+    // Redirect std::cout to capture the output
+    std::ostringstream output;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+    // Call the function to capture its output
+    graph.print_features();
+
+    // Restore std::cout
+    std::cout.rdbuf(oldCoutBuffer);
+
+    std::string expected_print =
+        "0\t1, 0, '#', 0, 0, 0\t2\n"
+        "1\t0, 1, '#', 0, 0, 0\t0\n"
+        "2\t0, 0, '#', 1, 0, 0\t1\n"
+        "3\t0, 0, '#', 0, 1, 0\t3\n"
+        "4\t0, 0, '#', 0, 0, 1\t4\n";
+
+    EXPECT_EQ(output.str(), expected_print);
+}
+
+TEST(GraphTest, print_double_features) {
+    std::string edges_path = "../input/test/graph/edges_example.txt";
+    std::string features_path = "../input/test/graph/features_example.txt";
+    Graph<double> graph(edges_path, features_path);
+
+    // Redirect std::cout to capture the output
+    std::ostringstream output;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
+
+    // Call the function to capture its output
+    graph.print_features();
+
+    // Restore std::cout
+    std::cout.rdbuf(oldCoutBuffer);
+
+    std::string expected_print =
+        "0\t1, 0, '#', 0, 0, 0\t2\n"
+        "1\t0, 1, '#', 0, 0, 0\t0\n"
+        "2\t0, 0, '#', 1, 0, 0\t1\n"
+        "3\t0, 0, '#', 0, 1, 0\t3\n"
+        "4\t0, 0, '#', 0, 0, 1\t4\n";
+
+    EXPECT_EQ(output.str(), expected_print);
 }
 
 TEST(GraphTest, get_neighbours) {
@@ -81,119 +294,6 @@ TEST(GraphTest, is_valid_node) {
 
     EXPECT_TRUE(graph.is_valid_node(0));
     EXPECT_FALSE(graph.is_valid_node(graph.get_num_nodes()));
-}
-
-TEST(GraphTest, print_edges) {
-    std::string edges_path = "../input/test/graph/edges_example.txt";
-    std::string features_path = "../input/test/graph/features_example.txt";
-    Graph<double> graph(edges_path, features_path);
-
-    // Redirect std::cout to capture the output
-    std::ostringstream output;
-    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
-
-    // Call the function to capture its output
-    graph.print_edges();
-
-    // Restore std::cout
-    std::cout.rdbuf(oldCoutBuffer);
-
-    std::string expected_print =
-        "1\t0\n"
-        "2\t1\n"
-        "3\t2\n"
-        "4\t3\n"
-        "4\t0\n";
-
-    EXPECT_EQ(output.str(), expected_print);
-}
-
-TEST(GraphTest, print_features) {  // TODO: Test for all feature data types
-    std::string edges_path = "../input/test/graph/edges_example.txt";
-    std::string features_path = "../input/test/graph/features_example.txt";
-    Graph graph(edges_path, features_path);
-
-    // Redirect std::cout to capture the output
-    std::ostringstream output;
-    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
-
-    // Call the function to capture its output
-    graph.print_features();
-
-    // Restore std::cout
-    std::cout.rdbuf(oldCoutBuffer);
-
-    std::string expected_print =
-        "0\t1, 0, '#', 0, 0, 0\t2\n"
-        "1\t0, 1, '#', 0, 0, 0\t0\n"
-        "2\t0, 0, '#', 1, 0, 0\t1\n"
-        "3\t0, 0, '#', 0, 1, 0\t3\n"
-        "4\t0, 0, '#', 0, 0, 1\t4\n";
-
-    EXPECT_EQ(output.str(), expected_print);
-}
-
-TEST(GraphTest, read_edges) {
-    std::string edges_path = "../input/test/graph/edges_example.txt";
-    std::string features_path = "../input/test/graph/features_example.txt";
-    Graph<double> graph(edges_path, features_path);
-
-    // Create temporary file for output
-    std::string temp_file_path = "temp_edges.txt";
-    {
-        std::ofstream output(temp_file_path);
-        // Redirect cout to the file
-        std::streambuf* old = std::cout.rdbuf(output.rdbuf());
-        graph.print_edges();
-        std::cout.rdbuf(old);
-    }
-
-    // Compare files line by line
-    std::ifstream file1(edges_path);
-    std::ifstream file2(temp_file_path);
-    std::string line1, line2;
-    bool files_match = true;
-    int line_number = 0;
-
-    while (std::getline(file1, line1) && std::getline(file2, line2)) {
-        line_number++;
-        if (line1 != line2) {
-            files_match = false;
-            break;
-        }
-    }
-
-    EXPECT_TRUE(files_match) << "Files differ at line " << line_number;
-
-    bool more_lines = std::getline(file1, line1) || std::getline(file2, line2);
-    EXPECT_FALSE(more_lines) << "Files have different lengths";
-
-    // Clean up
-    std::remove(temp_file_path.c_str());
-}
-
-TEST(GraphTest, read_features) {  // TODO: Test for all feature data types
-    std::string edges_path = "../input/test/graph/edges_example.txt";
-    std::string features_path = "../input/test/graph/features_example.txt";
-    Graph graph(edges_path, features_path);
-
-    // clang-format off
-    std::vector<std::vector<double>> actual_features = {
-        {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2},
-        {0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0},
-        {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1},
-        {0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 3},
-        {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 4}
-    };
-    // clang-format on
-
-    for (int node = 0; node < graph.get_num_nodes(); node++) {
-        for (int feature = 0; feature < graph.get_num_features(); feature++) {
-            if (!graph.is_missing(node, feature)) {
-                EXPECT_DOUBLE_EQ(graph.get_feature(node, feature), actual_features[node][feature]);
-            }
-        }
-    }
 }
 
 TEST(UtilityTest, remove_duplicates) {
