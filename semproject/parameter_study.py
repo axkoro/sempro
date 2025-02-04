f9b494073c6e898b9150686b5638cc364ad3ce7b
Alexandre Koroneos <akoroneos@outlook.de>
2025-02-04 19:38:14 +0100

    Update parameter study script

--- semproject/parameter_study.py
+++ semproject/parameter_study.py
@@ -6,108 +6,79 @@ import re
 import matplotlib.pyplot as plt
 
 def main():
-    # knn depth on twitch
+    plot_knn_depth()
+
+def plot_knn_depth():
     depths = [1, 2, 3, 4]
-    overlap_total = []
-    overlap_missing = []
-    error_max = []
-    error_avg = []
-    r2 = []
+    data_sets = ["twitch", "amazon", "genius", "github"] # "corafull" excluded due to current memory issues with evaluation script
 
-    for depth in depths:
-        bm.strat_benchmark("knn", "twitch", knn_depth=depth)
-        results = evaluate_strat("knn", "twitch")
-        overlap_total.append(results["overlap_total"])
-        overlap_missing.append(results["overlap_missing"])
-        error_max.append(results["error_max"])
-        error_avg.append(results["error_avg"])
-        r2.append(results["r2"])
+    results = {}
+    for data_set in data_sets:
+        overlap_total = []
+        overlap_missing = []
+        error_max = []
+        error_avg = []
+        r2 = []
 
-    plot_quality_measures(strategy_name="kNN",
-                          data_set_name="Twitch",
-                          parameter_name="depth",
-                          parameter_list=depths,
-                          overlap_total=overlap_total,
-                          overlap_missing=overlap_missing,
-                          error_max=error_max,
-                          error_avg=error_avg,
-                          r2=r2)
-    
-    # louvain
-    depths = [1, 2, 3, 4]
-    overlap_total = []
-    overlap_missing = []
-    error_max = []
-    error_avg = []
-    r2 = []
+        for depth in depths:
+            bm.strat_benchmark(strategy="knn", data_set=data_set, knn_depth=depth)
+            evaluation_results = run_evaluation(strat="knn", data_set=data_set)
 
-    for depth in depths:
-        bm.strat_benchmark("louvain", "twitch", knn_depth=depth)
-        results = evaluate_strat("knn", "twitch")
-        overlap_total.append(results["overlap_total"])
-        overlap_missing.append(results["overlap_missing"])
-        error_max.append(results["error_max"])
-        error_avg.append(results["error_avg"])
-        r2.append(results["r2"])
+            overlap_total.append(evaluation_results["overlap_total"])
+            overlap_missing.append(evaluation_results["overlap_missing"])
+            error_max.append(evaluation_results["error_max"])
+            error_avg.append(evaluation_results["error_avg"])
+            r2.append(evaluation_results["r2"])
+        
+        results[data_set] = {"overlap_total": overlap_total,
+                                "overlap_missing": overlap_missing,
+                                "error_max": error_max,
+                                "error_avg": error_avg,
+                                "r2": r2}
 
-    plot_quality_measures(strategy_name="kNN",
-                          data_set_name="Twitch",
-                          parameter_name="depth",
-                          parameter_list=depths,
-                          overlap_total=overlap_total,
-                          overlap_missing=overlap_missing,
-                          error_max=error_max,
-                          error_avg=error_avg,
-                          r2=r2)
-
-def plot_quality_measures(strategy_name, data_set_name, parameter_name, parameter_list, overlap_total, overlap_missing, error_max, error_avg, r2):
-    """
-    Plot quality measures versus kNN depth. Each measure is plotted in its own graph.
-
-    Args:
-        strategy_name (string): The name of strategy that was evaluated.
-        parameter_name (string): The name of the data set on which that strategy was run.
-        parameter_name (string): The name of the parameter to be plotted.
-        parameter_list (list): List of the different parameter values that were run and evaluated.
-        overlap_total (list): Overlap total percentages.
-        overlap_missing (list): Overlap missing percentages.
-        error_max (list): Maximum error values.
-        error_avg (list): Average error values.
-        r2 (list): R2 score values.
-    """
+    # Plot
     fig, axs = plt.subplots(nrows=2, ncols=2)
-    fig.suptitle(f"{strategy_name} - {parameter_name} ({data_set_name})")
-    plt.subplots_adjust(hspace=0.5)
-    plt.subplots_adjust(wspace=0.5)
+    fig.suptitle(f"kNN - Depth")
+    plt.subplots_adjust(hspace=0.5, wspace=0.5)
+
+    data_set_colors = {"twitch": "purple", "amazon" : "orange", "amazon_fraud": "red", "corafull" : "blue", "genius" : "yellow", "github" : "green"}
 
     axs = axs.flatten()
     for ax in axs:
-        ax.set_xlabel(f"{parameter_name}")
-        axs[0].set_xticks(parameter_list)
-        
+        ax.set_xlabel(f"Depth")
+        ax.set_xticks(depths)
 
-    axs[0].plot(parameter_list, overlap_missing, marker="o", color="orange")
+    for data_set in data_sets:
+        axs[0].plot(depths, results[data_set]["overlap_missing"], marker="o", color=data_set_colors[data_set])
     axs[0].set_ylabel("Overlap Missing (%)")
+    axs[0].legend()
 
-    axs[1].plot(parameter_list, error_max, marker="o", color="green")
+    # TODO: normalise this somehow (genius max error is 1e6 while amazon max error is 1 which becomes invisible on the diagram)
+    for data_set in data_sets:
+        axs[1].plot(depths, results[data_set]["error_max"], marker="o", color=data_set_colors[data_set])
     axs[1].set_ylabel("Maximum Error (abs)")
+    axs[1].legend()
 
-    axs[2].plot(parameter_list, error_avg, marker="o", color="red")
+    for data_set in data_sets:
+        axs[2].plot(depths, results[data_set]["error_avg"], marker="o", color=data_set_colors[data_set])
     axs[2].set_ylabel("Average Error (abs)")
+    axs[2].legend()
 
-    axs[3].plot(parameter_list, r2, marker="o", color="purple")
+    for data_set in data_sets:
+        axs[3].plot(depths, results[data_set]["r2"], marker="o", color=data_set_colors[data_set])
     axs[3].set_ylabel("R2 Score")
+    axs[3].legend()
 
-    plt.savefig(f"{strategy_name}_{parameter_name}_{data_set_name}.png".lower())
+    plt.savefig(f"knn_depth.png".lower(), dpi=300)
     plt.close(fig)
 
-def evaluate_strat(strat, data_set):
+def run_evaluation(strat, data_set):
     evaluation_cmd = [
         sys.executable,
         "./extlibs/evaluation/measure-quality.py",
-        "--instance", data_set,
+        "--instance", data_set.lower(),
         "--input-folder", "./data/input",
-        "--feature-folder", f"./data/output/{strat}",
+        "--feature-folder", f"./data/output/{strat.lower()}",
         "--reference-folder", "./data/reference",
         "--txt"
     ]
