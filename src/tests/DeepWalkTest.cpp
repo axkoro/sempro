49e03c726f7c7bdb8f8ad1d844646ef8eccd057a
Alexandre Koroneos <akoroneos@outlook.de>
2025-02-05 19:11:16 +0100

    Add test stubs for DeepWalk

---
+++ src/tests/DeepWalkTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(EdgeWeightCalculatorTest, test_name) {}
+
+TEST(RandomWalkGeneratorTest, test_name) {}
+
+TEST(SkipGramTest, test_name) {}
+
+TEST(DeepWalkImputerTest, test_name) {}
