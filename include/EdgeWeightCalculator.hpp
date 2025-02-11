c3dd6874a80cc505d5a5909821854311c0ce70b1
Marios Spathiadis <marios.spathiadis@gmail.com>
2025-02-11 19:23:03 +0100

    make compute_covers private and add friend test

--- include/EdgeWeightCalculator.hpp
+++ include/EdgeWeightCalculator.hpp
@@ -3,7 +3,9 @@
 #include "Graph.hpp"
 #include "WeightedGraph.hpp"
 #include <unordered_set>
+#define FRIEND_TEST(test_case_name, test_name) friend class test_case_name##_##test_name##_Test
 class EdgeWeightCalculator {
+    FRIEND_TEST(EdgeWeightCalculatorTest, ComputeCovers);
    private:
     Graph& graph;
     double fusion_coefficient;
@@ -11,12 +13,11 @@ class EdgeWeightCalculator {
 
    public:
     EdgeWeightCalculator(Graph& g, double fusion_coefficient);
-    std::vector<std::unordered_set<int>> compute_covers(WeightedGraph& wgraph);
     WeightedGraph generate_weighted_graph();
 
    private:
     double compute_weight(int u, int v,std::vector<std::unordered_set<int>> covers);
     double compute_feature_similarity(int u, int v);
     double compute_structural_similarity(int u, int v,std::vector<std::unordered_set<int>> covers);
-    
+    std::vector<std::unordered_set<int>> compute_covers(WeightedGraph& wgraph);
 };
