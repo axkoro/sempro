705faebd3ea25239597680a58b67632fdb770828
Alexandre Koroneos <akoroneos@outlook.de>
2025-02-12 19:55:37 +0100

    Adjust KNNImputer to templated Graph class

---
+++ include/Imputer.tpp
@@ -0,0 +1,28 @@
+#include <cmath>
+
+#include "Imputer.hpp"
+
+template <typename T>
+double Imputer<T>::compute_global_average(int feature) {
+    double sum = 0.0;
+    int count = 0;
+    for (int node = 0; node < graph.get_num_nodes(); node++) {
+        if (!graph.is_missing(node, feature)) {
+            sum += graph.get_feature(node, feature);
+            count++;
+        }
+    }
+
+    return (count > 0) ? (sum / count) : 0;
+}
+
+template <typename T>
+T round_value(double value) {
+    if constexpr (std::is_same_v<T, bool>) {
+        return (value >= 0.5);
+    } else if constexpr (std::is_integral_v<T> && !std::is_same_v<T, bool>) {
+        return static_cast<T>(std::round(value));
+    } else {
+        return static_cast<T>(value);
+    }
+}
--- include/KNNImputer.hpp
+++ include/KNNImputer.hpp
@@ -1,29 +1,20 @@
 #pragma once
 
-#include "GraphBool.hpp"
-#include "GraphDouble.hpp"
-#include "GraphInt.hpp"
 #include "Imputer.hpp"
 
-class KNNImputer : public Imputer {
-   public:
-    static constexpr int DEFAULT_K = 3;
-    enum feature_type { b, d, i };  // bool, double, int
+#define FRIEND_TEST(test_case_name, test_name) friend class test_case_name##_##test_name##_Test
+
+template <typename T>
+class KNNImputer : public Imputer<T> {
+    FRIEND_TEST(KNNTest, testGlobalAverage);
 
    private:
-    int k = DEFAULT_K;
-    feature_type type;
+    int depth;
 
    public:
-    explicit KNNImputer(GraphBool& g) : Imputer(g), type(b) {}
-    explicit KNNImputer(GraphDouble& g) : Imputer(g), type(d) {}
-    explicit KNNImputer(GraphInt& g) : Imputer(g), type(i) {}
+    KNNImputer(AttributedGraph<T>& g, int depth = 2) : Imputer<T>(g), depth(depth) {}
 
-    void run();
-
-    // Configuration
-    void set_depth(int k);
-
-    // getter for k
-    int get_depth();
+    void run() override;
 };
+
+#include "KNNImputer.tpp"
