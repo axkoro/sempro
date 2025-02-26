34d268534c4ba555682222ed83a9b8c1abc17c5d
Alexandre Koroneos <akoroneos@outlook.de>
2025-02-26 16:14:12 +0100

    Fix python build

--- src/pybind/py11_louvain.cpp
+++ src/pybind/py11_louvain.cpp
@@ -7,7 +7,7 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(cpp_louvain_module, m) {
     py::class_<Louvain>(m, "Louvain")
-        .def(py::init<const Graph&>(), py::arg("g"))
+        .def(py::init<const Graph&, const LouvainConfig&>(), py::arg("g"), py::arg("config"))
         .def("execute", &Louvain::execute)
         .def("get_modularity", &Louvain::get_modularity);
 }
