axkoro/sempro

commit 51ecaabd89b256e5e9f7a147e50a50ab901cc387

Minor refactor of DeepWalkImputer::impute_features()

include/DeepWalkImputer.tpp
@@ -27,54 +27,70 @@ void DeepWalkImputer<T>::run() {
     skip_gram.train(walks);
     const Matrix& embeddings = skip_gram.get_embeddings();
 
-    impute_features(embeddings, config.top_similar);
+    impute_features(embeddings);
 }
 
 template <typename T>
-void DeepWalkImputer<T>::impute_features(const Matrix& embeddings, int top_similar) {
+void DeepWalkImputer<T>::impute_features(const Matrix& embeddings) {
     int num_nodes = this->graph.get_num_nodes();
     int num_features = this->graph.get_num_features();
 
     for (int node = 0; node < num_nodes; ++node) {
-        for (int feature = 0; feature < num_features; ++feature) {
-            if (this->graph.is_missing(node, feature)) {
-                // Calculate the similarity ranking vector
-                std::vector<std::pair<int, double>> similarity_ranking;
-                for (int other_node = 0; other_node < num_nodes; ++other_node) {
-                    if (node != other_node) {
-                        double similarity = calculate_similarity(embeddings.get_row(node),
-                                                                 embeddings.get_row(other_node));
-                        similarity_ranking.push_back({other_node, similarity});
-                    }
-                }
+        auto missing_feature_indices = this->graph.get_missing_features(node);
+        if (missing_feature_indices.empty()) continue;
 
-                // Sort the similarity ranking
-                std::sort(similarity_ranking.begin(), similarity_ranking.end(),
-                          [](const std::pair<int, double>& a, const std::pair<int, double>& b) {
-                              return a.second > b.second;
-                          });
+        std::vector<std::pair<int, double>> similarity_ranking =
+            get_similarity_ranking(node, embeddings);
 
-                // Impute the missing feature using the top similar nodes
-                double imputed_value = 0.0;
-                int count = 0;
-                for (const auto& [similar_node, similarity] : similarity_ranking) {
-                    if (!this->graph.is_missing(similar_node, feature)) {
-                        imputed_value += this->graph.get_feature(similar_node, feature);
-                        count++;
-                        if (count == top_similar) break;  // Use the top n similar nodes
-                    }
-                }
-                if (count > 0) {
-                    T imputed = round_value<T>(imputed_value / count);
-                    this->graph.set_feature(node, feature, imputed);
-                    this->graph.set_missing(node, feature, false);
-                } else {
-                    // for now set value to 1
-                    this->graph.set_feature(node, feature, 1.0);
-                }
+        for (auto&& feature : missing_feature_indices) {
+            double feature_sum = 0.0;  // for average calculation
+            int count = 0;
+            for (const auto& [similar_node, similarity] : similarity_ranking) {
+                if (this->graph.is_missing(similar_node, feature)) continue;
+
+                feature_sum += this->graph.get_feature(similar_node, feature);
+                count++;
+                if (count == config.top_similar) break;  // Use the top n similar nodes
             }
+            if (count > 0) {
+                T imputed_val = round_value<T>(feature_sum / count);
+                this->graph.set_feature(node, feature, imputed_val);
+            } else {
+                this->graph.set_feature(node, feature, 1.0);
+            }
+            this->graph.set_missing(node, feature, false);
         }
     }
+}
+
+/**
+ * @return A std::vector of std::pair<int, double>, where pair.first is the index of the compared
+ * node and pair.second is the similarity value of this node and the input node that was passed. The
+ * vector is sorted by the similarity value.
+ */
+template <typename T>
+std::vector<std::pair<int, double>> DeepWalkImputer<T>::get_similarity_ranking(
+    int node, const Matrix& embeddings) {
+    int num_nodes = this->graph.get_num_nodes();
+
+    std::vector<std::pair<int, double>> similarity_ranking;
+    similarity_ranking.reserve(num_nodes - 1);
+
+    for (int other_node = 0; other_node < num_nodes; ++other_node) {
+        if (node != other_node) continue;
+
+        double similarity =
+            calculate_similarity(embeddings.get_row(node), embeddings.get_row(other_node));
+
+        similarity_ranking.emplace_back(other_node, similarity);
+    }
+
+    std::sort(similarity_ranking.begin(), similarity_ranking.end(),
+              [](const std::pair<int, double>& a, const std::pair<int, double>& b) {
+                  return a.second > b.second;
+              });
+
+    return similarity_ranking;
 }
 
 template <typename T>

include/DeepWalkImputer.hpp
@@ -15,9 +15,9 @@ class DeepWalkImputer : public Imputer<T> {
    private:
     DeepWalkConfig config;
     int seed;
-    int top_similar;
 
-    void impute_features(const Matrix& embeddings, int top_similar);
+    void impute_features(const Matrix& embeddings);
+    std::vector<std::pair<int, double>> get_similarity_ranking(int node, const Matrix& embeddings);
     double calculate_similarity(const Vector& vec1, const Vector& vec2);
 };
 

src/tests/DeepWalkTest.cpp
@@ -232,11 +232,9 @@ TEST(DeepWalkImputerTest, no_missing_features) {
     std::string features_file = "../data/test/deepwalk/test_missing_features.txt";
 
     AttributedGraph<double> graph(edges_file, features_file);
-    DeepWalkImputer<double> imputer(graph, -1);
+    DeepWalkImputer<double> imputer(graph);
     imputer.run();
 
-    // Check that no feature is missing for any node
-    int count = 0;
     for (int node = 0; node < graph.get_num_nodes(); ++node) {
         std::vector<int> missing_features = graph.get_missing_features(node);
         ASSERT_TRUE(missing_features.empty()) << "Node " << node << " has missing features.";

commit 31c3d90c6edc9a38017189d5be89269ec068ac86

Fix bug introduced in 51ecaab

include/DeepWalkImputer.tpp
@@ -17,6 +17,7 @@ template <typename T>
 void DeepWalkImputer<T>::run() {
     EdgeWeightCalculator<T> ew_calc(this->graph, config.fusion_coefficient);
     GraphEdgeWeights edge_weights = ew_calc.generate_edge_weights();
+    // GraphEdgeWeights edge_weights(this->graph); // sets every edge weight to 1
 
     RandomWalkGenerator rw_gen(this->graph, edge_weights, config.walk_length, config.num_walks,
                                seed);
@@ -53,7 +54,7 @@ void DeepWalkImputer<T>::impute_features(const Matrix& embeddings) {
                 if (count == config.top_similar) break;  // Use the top n similar nodes
             }
             if (count > 0) {
-                T imputed_val = round_value<T>(feature_sum / count);
+                T imputed_val = round_value<T>(feature_sum / static_cast<double>(count));
                 this->graph.set_feature(node, feature, imputed_val);
             } else {
                 this->graph.set_feature(node, feature, 1.0);
@@ -77,7 +78,7 @@ std::vector<std::pair<int, double>> DeepWalkImputer<T>::get_similarity_ranking(
     similarity_ranking.reserve(num_nodes - 1);
 
     for (int other_node = 0; other_node < num_nodes; ++other_node) {
-        if (node != other_node) continue;
+        if (node == other_node) continue;
 
         double similarity =
             calculate_similarity(embeddings.get_row(node), embeddings.get_row(other_node));